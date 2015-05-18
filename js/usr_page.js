8ed3944899f021aec6e23184ace5697c4f8a8e3f
xiaoze123 <455935725@qq.com>
2015-05-18 15:55:42 +0800

    块完成

--- js/usr_page.js
+++ js/usr_page.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
         $("form").show();
     });
 });
-$("#sld label input").click(function(){
-    $(this).parents("label").toggleClass("chd");
+$("#container_check_box label").click(function(){
+    if($(this).find("input").attr("checked")){
+        $(this).find("input").removeAttr("checked");
+    }else{
+        $(this).find("input").attr("checked","checked");
+    }
+    $(this).toggleClass("chd");
+    return false;
 })
+//$(document).ready(function(){
+//    $("input:checkbox").each(function(){
+//        $(this).click(function(){
+//            //alert($(this).attr('checked'));
+//            $(this).toggleClass('label_on');
+//        });
+//    });
+//});
