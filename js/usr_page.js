4acca0d6f53e2413c5ede0a8888b694821389e1e
xiaoze123 <455935725@qq.com>
2015-05-21 09:47:57 +0800

    完成

--- js/usr_page.js
+++ js/usr_page.js
@@ -7,20 +7,3 @@ $(document).ready(function(){
         $("form").show();
     });
 });
-$("#container_check_box label").click(function(){
-    if($(this).find("input").attr("checked")){
-        $(this).find("input").removeAttr("checked");
-    }else{
-        $(this).find("input").attr("checked","checked");
-    }
-    $(this).toggleClass("chd");
-    return false;
-})
-//$(document).ready(function(){
-//    $("input:checkbox").each(function(){
-//        $(this).click(function(){
-//            //alert($(this).attr('checked'));
-//            $(this).toggleClass('label_on');
-//        });
-//    });
-//});
